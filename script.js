4708e527a0348b993d9f79029cd5cb6897df201b
BergfexFabs <fabian.eder98@gmx.at>
2026-01-08 10:49:00 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const input = document.getElementById("itemInput");
-const button = document.getElementById("addBtn");
+const addBtn = document.getElementById("addBtn");
+const clearBtn = document.getElementById("clearDone");
 const list = document.getElementById("list");
 
-button.addEventListener("click", addItem);
+addBtn.addEventListener("click", addItem);
+clearBtn.addEventListener("click", clearDoneItems);
 
 function addItem() {
     if (input.value === "") return;
@@ -11,13 +13,19 @@ function addItem() {
     li.textContent = input.value;
 
     li.addEventListener("click", () => {
-        li.remove();
+        li.classList.toggle("done");
         saveList();
     });
 
     list.appendChild(li);
     input.value = "";
 
+    saveList();
+}
+
+function clearDoneItems() {
+    const items = document.querySelectorAll(".done");
+    items.forEach(item => item.remove());
     saveList();
 }
 
@@ -29,6 +37,14 @@ function saveList() {
 // ?? Laden
 function loadList() {
     list.innerHTML = localStorage.getItem("items") || "";
+
+    // Click-Listener nach dem Laden wieder setzen
+    document.querySelectorAll("li").forEach(li => {
+        li.addEventListener("click", () => {
+            li.classList.toggle("done");
+            saveList();
+        });
+    });
 }
 
 loadList();
